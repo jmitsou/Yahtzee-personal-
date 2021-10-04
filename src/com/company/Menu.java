89c171f575dec870567c0b9e41782cacee25deaa
Jeff Mitsou <shinetachi88@gmail.com>
2021-10-03 22:18:37 -0400

    worked on Menu class

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class Menu {
+
+    public String welcomeMessage;
+    public String rules;
+
+    public void showWelcome(){
+
+    }
+
+    public void showRules(){
+
+    }
+
+    public void start(){
+
+    }
 }
